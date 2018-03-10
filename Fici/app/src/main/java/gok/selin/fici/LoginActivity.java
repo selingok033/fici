a5aa2dbb9200b202d6468539ef736a58e872513e
BerkayErgun <berkay.ergun@hotmail.com>
2018-03-10 16:40:13 +0300

    Login sayfası oluşturuldu.

--- Fici/app/src/main/java/gok/selin/fici/LoginActivity.java
+++ Fici/app/src/main/java/gok/selin/fici/LoginActivity.java
@@ -1,7 +1,14 @@
 package gok.selin.fici;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 /*
 BERKAY
@@ -11,10 +18,38 @@ lazım. Bunu Intent ile yapacaksın. Web'te "başka bir activity başlatmak" diy
 */
 
 public class LoginActivity extends AppCompatActivity {
-
+    ImageView logo;
+    EditText name,pass;
+    Button buttongiris;
+    String username,userpass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        logo = findViewById(R.id.imageView);
+        name = findViewById(R.id.editTextUsername);
+        pass = findViewById(R.id.editTextPass);
+        buttongiris =findViewById(R.id.buttonGiris);
+
+        buttongiris.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                username = name.getText().toString();
+                userpass = pass.getText().toString();
+                Toast.makeText(getApplicationContext(),"Name: "+username+" Pass: "+userpass,Toast.LENGTH_LONG).show();
+
+                if(username.equals("admin") && userpass.equals("root"))
+                {
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    intent.putExtra("USERNAME",username);
+                    startActivity(intent);
+                }
+
+
+            }
+        });
+
     }
 }
