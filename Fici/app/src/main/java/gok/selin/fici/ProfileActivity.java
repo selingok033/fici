ce8408b843832e3a4dbb6ebded73317943489810
Deleroth <simge.toksoz@gmail.com>
2018-03-17 15:28:52 +0300

    First commit from Umut. Simple spinners for ingredients and equipment, and a profile image is added.

--- Fici/app/src/main/java/gok/selin/fici/ProfileActivity.java
+++ Fici/app/src/main/java/gok/selin/fici/ProfileActivity.java
@@ -2,6 +2,8 @@ package gok.selin.fici;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /*
 UMUT
@@ -16,5 +18,20 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+
+        Spinner ingredientsSpinner = (Spinner) findViewById(R.id.ingredientSpinner);
+        Spinner equipmentSpinner = (Spinner) findViewById(R.id.equipmentSpinner);
+        ArrayAdapter <CharSequence> ingredientsAdapter = ArrayAdapter
+                .createFromResource(this, R.array.ingredients,android.R.layout.simple_spinner_item);
+        
+        ArrayAdapter <CharSequence> equipmentAdapter = ArrayAdapter
+                .createFromResource(this,R.array.equipment,android.R.layout.simple_spinner_item);
+
+        ingredientsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        equipmentAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        ingredientsSpinner.setAdapter(ingredientsAdapter);
+        equipmentSpinner.setAdapter(equipmentAdapter);
     }
 }
